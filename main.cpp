1dcc528e28677a28c306d5ea55bccdafd68a978a
Alvaro Cruz <alvarocruzbrusi@gmail.com>
2026-02-17 22:03:35 -0500

    Ammo, health, armor

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 #include "util/memory.h"
 #include "util/offsets.h"
 
+Memory memory;
+
+void SetHealth(uintptr_t value) {
+	uintptr_t localPlayer = memory.Read(LOCAL_PLAYER_OFFSET);
+	memory.Write(localPlayer + HEALTH_OFFSET, value);
+}
+
+void SetPrimaryAmmo(uintptr_t value) {
+	uintptr_t localPlayer = memory.Read(LOCAL_PLAYER_OFFSET);
+	memory.Write(localPlayer + PRIMARY_AMMO_OFFSET, value);
+}
+
+void SetSecondaryAmmo(uintptr_t value) {
+	uintptr_t localPlayer = memory.Read(LOCAL_PLAYER_OFFSET);
+	memory.Write(localPlayer + SECONDARY_AMMO_OFFSET, value);
+}
+
+void SetArmor(uintptr_t value) {
+	uintptr_t localPlayer = memory.Read(LOCAL_PLAYER_OFFSET);
+	memory.Write(localPlayer + ARMOR_OFFSET, value);
+}
+
 void injected_thread() {
 	Memory memory = Memory();
 	while (true) {
-		if (GetAsyncKeyState('M')) {
-			uintptr_t localPlayer = memory.Read(LOCAL_PLAYER_OFFSET);
-			memory.Write(localPlayer + HEALTH_OFFSET, 69);
-		}
+		
+		SetHealth(999);
+		SetPrimaryAmmo(999);
+		SetSecondaryAmmo(999);
+		SetArmor(999);
 
 		Sleep(100);
 	}
--- util/offsets.h
+++ util/offsets.h
@@ -1,4 +1,8 @@
 #pragma once
 
+// local player
 #define LOCAL_PLAYER_OFFSET 0x10F4F4
 #define HEALTH_OFFSET 0xF8
+#define PRIMARY_AMMO_OFFSET 0x150
+#define SECONDARY_AMMO_OFFSET 0x13C
+#define ARMOR_OFFSET 0xFC
